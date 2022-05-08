f71bfb55958fbd6239879eddc9bd2865a0761f57
Gabriel Lucena <gnllucena@gmail.com>
2022-05-08 05:35:38 -0300

    Deploy script.

--- new/deploy.ps1
+++ new/deploy.ps1
@@ -1,23 +1,25 @@
+$DEPLOY_PATH = "D:/iis/microfrontends/"
+
 Write-Host "BUILDING APP FIRST" -ForegroundColor Green -BackgroundColor White -nonewline;
 npm --prefix app-first run build:webpack
 
 Write-Host "DEPLOYING APP FIRST" -ForegroundColor Green -BackgroundColor White -nonewline;
-cp ./app-first/dist/semnome-app-first.js D:/iis/microfrontends/
+cp ./app-first/dist/semnome-app-first.js $DEPLOY_PATH
 
 Write-Host "BUILDING APP SECOND" -ForegroundColor Green -BackgroundColor White -nonewline;
 npm --prefix app-second run build:webpack
 
 Write-Host "DEPLOYING APP SECOND" -ForegroundColor Green -BackgroundColor White -nonewline;
-cp ./app-second/dist/semnome-app-second.js D:/iis/microfrontends/
+cp ./app-second/dist/semnome-app-second.js $DEPLOY_PATH
 
 Write-Host "BUILDING NAVIGATION BAR" -ForegroundColor Green -BackgroundColor White -nonewline;
 npm --prefix navigation run build:webpack
 
 Write-Host "DEPLOYING NAVIGATION BAR" -ForegroundColor Green -BackgroundColor White -nonewline;
-cp ./navigation/dist/semnome-navigation.js D:/iis/microfrontends/
+cp ./navigation/dist/semnome-navigation.js $DEPLOY_PATH
 
 Write-Host "BUILDING STORYBOOK" -ForegroundColor Green -BackgroundColor White -nonewline;
 npm --prefix storybook run build:webpack
 
 Write-Host "DEPLOYING STORYBOOK" -ForegroundColor Green -BackgroundColor White -nonewline;
-cp ./storybook/dist/semnome-storybook.js D:/iis/microfrontends/
+cp ./storybook/dist/semnome-storybook.js $DEPLOY_PATH
--- new/root/src/semnome-root-config.ts
+++ new/root/src/semnome-root-config.ts
@@ -1,9 +1,5 @@
 import { registerApplication, start } from "single-spa";
-import {
-  constructApplications,
-  constructRoutes,
-  constructLayoutEngine,
-} from "single-spa-layout";
+import { constructApplications, constructRoutes, constructLayoutEngine } from "single-spa-layout";
 
 const routes = constructRoutes(document.querySelector("#single-spa-layout"));
 
@@ -19,9 +15,4 @@ const layoutEngine = constructLayoutEngine({ routes, applications });
 applications.forEach(registerApplication);
 layoutEngine.activate();
 
-// System.import("@react-mf/styleguide").then(() => {
-//   // Activate the layout engine once the styleguide CSS is loaded
-//   layoutEngine.activate();
-//   start();
-// });
 start();
